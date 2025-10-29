b4a6b53f893f5f85849c6c28e8a6f6c2c6f2425d
gratefulboi <bryanyung2005@gmail.com>
2025-10-29 16:28:20 +0700

    Commit No. 1 – add first & second worker

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.lab_week_08
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
+
+    // Buat instance work manager, work manager bertugas mengatur request dan workers. HANYA BOLEH ADA 1 dalam 1 aplikasi
+    private val workManager = WorkManager.getInstance(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,54 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(
+                getIdInputData(FirstWorker.INPUT_DATA_ID, id)
+            ).build()
+
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(
+                getIdInputData(SecondWorker.INPUT_DATA_ID, id)
+            ).build()
+
+        // Yang mengatur urutan request dari work manager. FirstWorker mulai dulu abistu second worker
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) {info ->
+                if (info != null && info.state.isFinished) {
+                    showResult("First process is done")
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) {info ->
+                if (info != null && info.state.isFinished) {
+                    showResult("Second process is done")
+                }
+            }
+
+
     }
+        private fun getIdInputData(idKey: String, idValue: String) =
+            Data.Builder()
+                .putString(idKey, idValue)
+                .build()
+
+        private fun showResult(message: String) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
 }
